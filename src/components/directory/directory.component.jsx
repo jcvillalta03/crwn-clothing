a0d4f07866e658e226f6aee413a4389039e13d1d
Julio Cesar Villalta III <jvillalta@nvisia.com>
2019-12-24 12:33:47 -0600

    hover and styling to menu-item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {this.state.sections.map(({ title, id, imageUrl, size, linkUrl }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}/>
         ))}
       </div>
     );
