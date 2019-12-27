2ee7e8ccb9b7683a9f9a2d962f3229293f4caa04
Julio Cesar Villalta III <jvillalta@nvisia.com>
2019-12-27 12:30:43 -0600

    persist state to local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,29 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+/**
+ * Make a persistence configuration object naming the key to store in,
+ * as well as reducers we wish to store in local storage
+ */
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"] // call out reducers we wish to persist into local storage
+};
+
+/**
+ * save our root reducer into an object
+ */
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+/**
+ * export out a persist reducer that combines our root reducer with our persistence configuration
+ */
+export default persistReducer(persistConfig, rootReducer);
